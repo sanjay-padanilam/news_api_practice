6628546aa1180e43011e963a1eb5413d70284623
sanjay-padanilam <sanjaysaji193@gmail.com>
2024-10-28 10:28:44 +0530

    finished

--- lib/controller/news_screen_controler.dart
+++ lib/controller/news_screen_controler.dart
@@ -4,8 +4,11 @@ import 'package:news_api_practice/model/news_model.dart';
 
 class NewsScreenControler with ChangeNotifier {
   List<Article> articlelist = [];
+  bool isloading = false;
 
   Future<void> getarticle() async {
+    isloading = true;
+    notifyListeners();
     final url = Uri.parse(
         "https://newsapi.org/v2/everything?q=bitcoin&apiKey=41f5562ab4a641069301792a794fa8a9");
     try {
@@ -14,6 +17,10 @@ class NewsScreenControler with ChangeNotifier {
         Newsmodel modelobj = newsmodelFromJson(response.body);
         articlelist = modelobj.articlelist ?? [];
       }
-    } catch (e) {}
+      notifyListeners();
+    } catch (e) {
+      print(e);
+    }
+    isloading = false;
   }
 }
--- lib/view/news_screen/news_screen.dart
+++ lib/view/news_screen/news_screen.dart
@@ -33,50 +33,55 @@ class _NewsScreenState extends State<NewsScreen> {
       backgroundColor: Colors.white,
       body: Center(
           child: Consumer<NewsScreenControler>(
-              builder: (context, proobj, child) => ListView.separated(
-                  itemBuilder: (context, index) => Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-                        color: Colors.grey,
-                        child: Column(
-                          children: [
-                            Text(
-                              "${proobj.articlelist[index].title.toString()}.",
-                              style: TextStyle(
-                                  fontSize: 25, fontWeight: FontWeight.bold),
+              builder: (context, proobj, child) => proobj.isloading
+                  ? CircularProgressIndicator()
+                  : ListView.separated(
+                      itemBuilder: (context, index) => Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 25),
+                            color: Colors.grey,
+                            child: Column(
+                              children: [
+                                Text(
+                                  "${proobj.articlelist[index].title.toString()}.",
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Container(
+                                  height: 200,
+                                  width: 300,
+                                  child: Image(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(proobj
+                                          .articlelist[index].urlToImage
+                                          .toString())),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                SizedBox(
+                                  height: 50,
+                                  child: Text(
+                                      "publish at ${proobj.articlelist[index].publishedAt}"),
+                                ),
+                                Text(
+                                  proobj.articlelist[index].description
+                                      .toString(),
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
                             ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Container(
-                              height: 200,
-                              width: 300,
-                              child: Image(
-                                  fit: BoxFit.cover,
-                                  image: NetworkImage(proobj
-                                      .articlelist[index].urlToImage
-                                      .toString())),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            SizedBox(
-                              height: 50,
-                              child: Text(
-                                  "publish at ${proobj.articlelist[index].publishedAt}"),
-                            ),
-                            Text(
-                              proobj.articlelist[index].description.toString(),
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                      ),
-                  separatorBuilder: (context, index) => SizedBox(
-                        height: 10,
-                      ),
-                  itemCount: proobj.articlelist.length))),
+                          ),
+                      separatorBuilder: (context, index) => SizedBox(
+                            height: 10,
+                          ),
+                      itemCount: proobj.articlelist.length))),
     );
   }
 }
